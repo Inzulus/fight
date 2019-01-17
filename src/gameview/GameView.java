02157c716329b48f5dd485392ffd154466d53ffa
Julius <inzulus@gmail.com>
2019-01-17 12:07:42 +0100

    moving player

--- src/gameview/GameView.java
+++ src/gameview/GameView.java
@@ -18,7 +18,9 @@ import java.util.ArrayList;
 public class GameView extends BorderPane {
     private Rectangle rec1;
     private Label highscoreLabel;
-    private Rectangle player;
+    private Rectangle bottomBar;
+    private Rectangle topBar;
+    protected Rectangle player;
     private Pane gameArea;
     protected Button spawnEnemy = new Button("spawnEnemy");
     protected Button fire = new Button("fire");
@@ -30,9 +32,11 @@ public class GameView extends BorderPane {
         enemyList = new ArrayList<Rectangle>();
         rec1 = new Rectangle(20,20);
         highscoreLabel = new Label("XXXXX");
-        player = new Rectangle(0,500,500,10);
+        bottomBar = new Rectangle(0,700,1920,2);//630 ist feldhöhe
+        topBar = new Rectangle(0,0,1920,2);
+        player = new Rectangle(800,600,30,30);
 
-        gameArea.getChildren().addAll(rec1,player);
+        gameArea.getChildren().addAll(topBar, bottomBar,player);
 
         HBox topBox = new HBox();
         topBox.getChildren().addAll(highscoreLabel,spawnEnemy,fire,menu);
@@ -42,38 +46,41 @@ public class GameView extends BorderPane {
     }
 
     public void addEnemyRectangle(){
-        Rectangle rectangle = new Rectangle((int)(Math.random()*500),0,20,20);
+        Rectangle rectangle = new Rectangle((int)(Math.random()*1920),0,20,20);
         enemyList.add(rectangle);
         gameArea.getChildren().add(rectangle);
 
         TranslateTransition translateTransition = new TranslateTransition();
         translateTransition.setDuration(Duration.millis(9000));
         translateTransition.setNode(rectangle);
-        translateTransition.setByY(300);
+        translateTransition.setByY(710);
         translateTransition.setCycleCount(1);
         translateTransition.setAutoReverse(false);
         rectangle.translateYProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 //System.out.println("changed");
-                collision(rectangle,player);
+                collision(rectangle, bottomBar);
             }
         });
         translateTransition.play();
     }
 
-    public void collision(Rectangle rectangle1, Rectangle rectangle2){
-        Bounds bound1 = rectangle1.getBoundsInParent();
+    public void collision(Rectangle enemy, Rectangle rectangle2){
+        Bounds bound1 = enemy.getBoundsInParent();
         Bounds bound2 = rectangle2.getBoundsInParent();
         if(bound1.intersects(bound2)){
-            rectangle1.setFill(Color.PINK);
+            enemy.setFill(Color.PINK);
             //translateTransition.stop();
+            gameArea.getChildren().remove(enemy);
+            //enemyList.remove(enemy);
+            System.out.println(enemyList.size());
         }
     }
 
     public void fireProjectile(int posX){
         System.out.println(posX);
-        Rectangle projectile = new Rectangle(posX,400,3,3);
+        Rectangle projectile = new Rectangle(posX,600,3,3);
         projectile.setStrokeWidth(0);
         projectile.setFill(Color.RED);
         gameArea.getChildren().add(projectile);
@@ -81,13 +88,14 @@ public class GameView extends BorderPane {
         TranslateTransition translateTransition = new TranslateTransition();
         translateTransition.setDuration(Duration.millis(9000));
         translateTransition.setNode(projectile);
-        translateTransition.setByY(-300);
+        translateTransition.setByY(-700);
         translateTransition.setCycleCount(1);
         translateTransition.setAutoReverse(false);
         projectile.translateYProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 //System.out.println("changed");
+                collision(projectile,topBar);
                 for(Rectangle enemy: enemyList){
                     collision(enemy,projectile);
                 }
--- src/gameview/GameViewController.java
+++ src/gameview/GameViewController.java
@@ -2,6 +2,9 @@ package gameview;
 
 import application.Main;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import player.MP3Player;
 
 public class GameViewController {
@@ -14,14 +17,35 @@ public class GameViewController {
         this.player = player;
         this.application = application;
         gameView = new GameView();
-        gameView.spawnEnemy.addEventHandler(ActionEvent.ACTION, event -> {
+        /*gameView.spawnEnemy.addEventHandler(ActionEvent.ACTION, event -> {
             gameView.addEnemyRectangle();
         });
         gameView.fire.addEventHandler(ActionEvent.ACTION,event -> {
-            gameView.fireProjectile((int)(Math.random()*500));
+            gameView.fireProjectile((int)(Math.random()*1920));
         });
         gameView.menu.addEventHandler(ActionEvent.ACTION,event -> {
             application.switchView("mainView");
+        });
+        gameView.addEventHandler(KeyEvent.KEY_PRESSED,event -> {
+            if(event.getCode() == KeyCode.SPACE){
+                gameView.fireProjectile((int)(Math.random()*1920));
+            }
+        });*/
+        gameView.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCode() == KeyCode.H) {
+                    gameView.fireProjectile((int) gameView.player.getX()+12);
+                }
+                if(event.getCode()== KeyCode.LEFT){
+                    gameView.player.setX(gameView.player.getX()-20);
+                }
+
+                if(event.getCode()== KeyCode.RIGHT){
+                    gameView.player.setX(gameView.player.getX()+20);
+                }
+
+            }
         });
     }
 
